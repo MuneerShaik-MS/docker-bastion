943427a0a5ab03391fef1bfdd31f543b9c249eb0
Muneer Shaik <shaik.muneer@zohomail.in>
2026-08-29 22:49:11 +0530

    docker bastion

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "6.61.0"
     }
   }
+  backend "s3" {
+    bucket = "cloudk-remote-state"
+    key    = "eks-bastion"
+    region = "us-east-1"
+    dynamodb_table = "cloudK-locking"
+  }
 }
 
 provider "aws" {
